21a152efc727447e7e0071414a3d9edaddb82a5d
Vegmot <anthony.yh.jang@gmail.com>
2020-12-07 13:45:22 -0800

    register available, hashing password with middleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -34,6 +34,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// encrypting the password
+// this should take place (pre-take place) before 'save'
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10); // 10 is default
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
