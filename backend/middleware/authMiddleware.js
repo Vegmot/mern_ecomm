b4dd767483a3733e782c8f4c536df6ab777148fa
Vegmot <anthony.yh.jang@gmail.com>
2020-12-09 13:23:06 -0800

    added admin middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -31,4 +31,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401); // not authorised
+    throw new Error('Not authorised as an admin');
+  }
+};
+
+export { protect, admin };
